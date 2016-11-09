f29f75aab0c51cb9e091f8d948793f3a4d21d2d5
Davikin <davikin92@gmail.com>
2016-11-09 16:05:20 -0600

    Añado tipografia y hago script para poner texto en cada local

---
+++ MapaOutlet/Assets/Buttons.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        foreach (Transform child in transform){
+            if (child.gameObject.name.ToLower().Contains("button") && !child.gameObject.name.ToLower().Contains("poligonal")){
+                GameObject buttonText = Instantiate(Resources.Load("buttonText"), child) as GameObject;
+                buttonText.transform.SetParent(child.transform);
+                RectTransform buttonTextRect = buttonText.GetComponent<RectTransform>();
+                buttonTextRect.offsetMin = new Vector2(0f, 0f);
+                buttonTextRect.offsetMax = new Vector2(0f, 0f);
+                if (child.gameObject.name.Contains("vertical") || child.gameObject.name.Contains("terraza")){
+                    Text textComponent = buttonText.GetComponent<Text>();
+                    textComponent.resizeTextForBestFit = true;
+                    if (!child.gameObject.name.Contains("terraza")){
+                        buttonTextRect.eulerAngles = new Vector3(0, 0, 90f);
+                    }
+                    else{
+                        textComponent.resizeTextMaxSize = 27;
+                    }
+                }
+                buttonText.transform.localPosition = new Vector3(buttonText.transform.localPosition.x, buttonText.transform.localPosition.y, 0);
+            }
+        }
+	}
+
+    void Update(){
+        
+    }
+}
---
+++ MapaOutlet/Assets/PopManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopManager : MonoBehaviour {
+
+    public static PopManager Instance { get; private set; }
+    public GameObject panel;
+
+    // Use this for initialization
+    void Awake () {
+        Instance = this;
+	}
+	
+    void Update(){
+        if (panel == null && GameObject.FindGameObjectWithTag("panel") != null){
+            panel = GameObject.FindGameObjectWithTag("panel");
+            panel.SetActive(false);
+        }
+    }
+
+	public void TogglePopUp(int storeIndex, bool showOrHide){
+         if(panel != null)
+            panel.SetActive(showOrHide);
+        //LoadInfo(storeIndex); //get from XML
+    }
+}
