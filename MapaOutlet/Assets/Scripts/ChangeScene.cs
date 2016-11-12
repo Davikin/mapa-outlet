35db5149d7c38a1f077723ea36e0d5f7763baa05
Davikin <davikin92@gmail.com>
2016-11-11 21:59:40 -0600

    añado XMLLoader, cambio de escena y prefabs para el poblado automatico con texto en los botones

---
+++ MapaOutlet/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Invoke("NextScene", 0.1f);
+    }
+
+    void NextScene() {
+        print("Loading next scene. Current scene is " + SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ MapaOutlet/Assets/Scripts/XMLLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class XMLLoader : MonoBehaviour {
+    TextAsset textAsset;
+    XmlDocument xmldoc;
+
+    void Start() {
+        textAsset = (TextAsset)Resources.Load("tiendas");
+        xmldoc = new XmlDocument();
+        xmldoc.LoadXml(textAsset.text);
+    }
+
+}
