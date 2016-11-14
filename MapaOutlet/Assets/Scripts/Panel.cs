90dcff63b2049c4b3991552bbdb322a22be938dd
Davikin <davikin92@gmail.com>
2016-11-14 12:41:51 -0600

    Añado codigo para encender y apagar la imagen blureada junto con el panel

--- MapaOutlet/Assets/Scripts/Panel.cs
+++ MapaOutlet/Assets/Scripts/Panel.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Panel : MonoBehaviour {
 
     public static Panel Instance { get; private set; }
-    public GameObject panel;
+    public GameObject panel, buttons;
 
     // Use this for initialization
     void Awake() {
@@ -16,9 +16,13 @@ public class Panel : MonoBehaviour {
             panel = GameObject.FindGameObjectWithTag("panel");
             panel.SetActive(false);
         }
+        if (buttons == null && GameObject.FindGameObjectWithTag("buttons") != null) {
+            buttons = GameObject.FindGameObjectWithTag("buttons");
+        }
     }
 
     public void TogglePanel(int infoIndex) {
         panel.SetActive(!panel.activeSelf);
+        buttons.SetActive(!panel.activeSelf);
     }
 }
