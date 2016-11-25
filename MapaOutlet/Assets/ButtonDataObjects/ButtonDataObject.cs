515493defa5da5b70cd23aca22c77a705bf9b277
Davikin <davikin92@gmail.com>
2016-11-24 18:54:29 -0600

    -Creo un Editor Script que crea un ButtonDataObject a partir del nombre de cada tienda, siguiendo la sintaxis de "." y "+"... -Hace falta mejorar el Editor Script para que le asigne los valores correspondientes a cada ButtonDataObject al crearlo

--- MapaOutlet/Assets/ButtonDataObjects/ButtonDataObject.cs
+++ MapaOutlet/Assets/ButtonDataObjects/ButtonDataObject.cs
@@ -9,4 +9,10 @@ public class ButtonDataObject : ScriptableObject {
     public string local;
     public string nombreVideo;
 
+    //constructor para cambiar las variables al crear cada instancia
+    public void Init(string _nombreTienda, string _numero, string _local) {
+        this.nombreTienda = _nombreTienda;
+        this.numero = _numero;
+        this.local = _local;
+    }
 }
---
+++ MapaOutlet/Assets/Editor/MultiButtonData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MultiButtonData {
+    
+    [MenuItem("Assets/Create/YourClass")]
+    public static void CreateAsset() {
+        Transform botones = GameObject.Find("Botones").transform;
+        string storeName;
+
+        foreach (Transform child in botones) {
+            storeName = child.name;
+            storeName = storeName.Substring(storeName.IndexOf(".")+1);
+            if(storeName.Contains("+")) storeName = storeName.Remove(storeName.IndexOf("+"));
+            if(storeName.Contains("'")) storeName = storeName.Replace("'","");
+            ScriptableObjectUtility.CreateAsset<ButtonDataObject>(storeName);
+        }
+    }
+}
