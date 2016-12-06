0cf8ae2244bb777b10855191e50e7c8b1f4915ba
Davikin <davikin92@gmail.com>
2016-12-06 17:05:39 -0600

    -Utilizo la clase WWW para cargar un XML tras hacer build, esto significa que pueden hacérsele cambios al XML y verlos reflejados en la aplicacion durante el siguiente run.
    +Falta integrar los nuevos logos enviados.
    +Falta implementar la funcionalidad WWW en el main project

--- TestXML/Assets/MyLoader.cs
+++ TestXML/Assets/MyLoader.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MyLoader : MonoBehaviour {
 
     public GameObject tcObj;
     TiendaContainer tc;
+    public Text debugText;
 
     // Use this for initialization
     void Start() {
         StartCoroutine(Load());
         
+        /* //It won't work here because hasn't loaded yet at this point
         foreach(Tienda tienda in tc.tiendas) {
             print("La tienda es "+tienda.nombre);
-        }
+        }*/
     }
 
     // Update is called once per frame
@@ -24,6 +27,11 @@ public class MyLoader : MonoBehaviour {
         WWW www = new WWW("file://C:/Users/DAVID BECERRA/AppData/LocalLow/Bromio/Mapa Outlet/BaseDeDatos_tiendas.xml");
         yield return www;
         tc = TiendaContainer.Load(www);
-        print(www.text);
+        int i = 0;
+        foreach (Tienda tienda in tc.tiendas) {
+            debugText.text += tienda.nombre + ". Telefono: " + tienda.phone + "\t";
+            i++;
+            if (i % 3 == 0) debugText.text += "\n";
+        }
     }
 }
